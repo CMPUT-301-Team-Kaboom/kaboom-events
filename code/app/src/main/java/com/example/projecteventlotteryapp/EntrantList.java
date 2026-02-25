36c00c12904c25995d6b95a371c14fb10b4e0974
Ben B <bbui@ualberta.ca>
2026-02-25 10:37:17 -0700

    added getters and setters to entrantList

--- code/app/src/main/java/com/example/projecteventlotteryapp/EntrantList.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/EntrantList.java
@@ -7,14 +7,26 @@ import java.util.ArrayList;
  * ex Waitlist, Invited, Enrolled, Declined
  */
 public class EntrantList {
-    private ArrayList<Entrant>  entrants;
+    private ArrayList<Entrant> entrants;
     private int listLength;
+
+    public EntrantList() {
+        entrants = new ArrayList<Entrant>();
+        listLength = 0;
+    }
 
     public ArrayList<Entrant> getEntrants() {
         return entrants;
     }
 
-    public void setEntrants(ArrayList<Entrant> entrants) {
-        this.entrants = entrants;
+    public void addEntrant(Entrant entrant) {
+        entrants.add(entrant);
+        listLength++;
+    }
+
+//    public popEntrant(Entrant entrant)
+
+    public int getListLength() {
+        return listLength;
     }
 }
