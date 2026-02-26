1d01d247a4666b3963a2c90c90297afc15d7d3c2
Ben B <bbui@ualberta.ca>
2026-02-26 13:41:32 -0700

    updated logic within fragment class

--- code/app/src/main/java/com/example/projecteventlotteryapp/CreateEventDialogFragment.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/CreateEventDialogFragment.java
@@ -1,14 +1,25 @@
 package com.example.projecteventlotteryapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
-import android.view.LayoutInflater;
+import android.os.Trace;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * A simple {@link DialogFragment} subclass.
@@ -16,39 +27,93 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CreateEventDialogFragment extends DialogFragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public CreateEventDialogFragment() {
-        // Required empty public constructor
+    interface CreateEventDialogListener {
+        void addEvent(Event event);
     }
-
-    public static CreateEventDialogFragment newInstance(String param1, String param2) {
-        CreateEventDialogFragment fragment = new CreateEventDialogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private CreateEventDialogListener listener;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof  CreateEventDialogListener) {
+            listener = (CreateEventDialogListener) listener;
+        } else {
+            throw new RuntimeException("Implement Listener");
         }
     }
 
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        // Inflate the same layout as before
-        return inflater.inflate(R.layout.fragment_create_event, container, false);
+    public Dialog onCreateDialog(@Nullable Bundle savedINstanceState) {
+        View view = getLayoutInflater().inflate(R.layout.fragment_create_event, null);
+
+        // grab references to editTexts and confirm button
+        EditText editName = view.findViewById(R.id.et_event_creation_name);
+        EditText editRegStart = view.findViewById(R.id.et_event_creation_registration_start);
+        EditText editRegEnd = view.findViewById(R.id.et_event_creation_registration_end);
+        EditText editDrawDate = view.findViewById(R.id.et_event_creation_draw_date);
+        EditText editDrawTime = view.findViewById(R.id.et_event_creation_draw_time);
+        EditText editEntrantLimit = view.findViewById(R.id.et_event_creation_entrant_limit);
+        Button confirmButton = view.findViewById(R.id.btn_event_creation_confirm);
+
+        Event event;
+
+        AlertDialog dialog = new AlertDialog.Builder(getContext())
+                .setView(view)
+                .create();
+
+        confirmButton.setOnClickListener(v -> {
+            String name = editName.getText().toString().trim();
+
+            // Validation
+            boolean isValid = true;
+            isValid &= fieldNotEmpty(editName);
+            isValid &= fieldNotEmpty(editRegStart);
+            isValid &= fieldNotEmpty(editRegEnd);
+            isValid &= fieldNotEmpty(editDrawDate);
+            isValid &= fieldNotEmpty(editDrawTime);
+            isValid &= fieldNotEmpty(editEntrantLimit);
+
+            // return after field checks
+            if (!isValid) {
+                return;
+            }
+
+            LocalDate regStart = LocalDate.parse(editRegStart.getText().toString().trim());
+            LocalDate regEnd = LocalDate.parse(editRegEnd.getText().toString().trim());
+            LocalDate drawDate = LocalDate.parse(editDrawDate.getText().toString().trim());
+            LocalTime drawTime = LocalTime.parse(editDrawTime.getText().toString().trim());
+            LocalDateTime drawDateTime = LocalDateTime.of(drawDate, drawTime);
+            int entrantLimit = Integer.parseInt(editEntrantLimit.getText().toString().trim());
+
+            if (!regEnd.isAfter(regStart)) {
+                editRegEnd.setError("Must be after start date");
+                isValid = false;
+            }
+
+            if (!drawDateTime.isAfter(regEnd)) {
+                editDrawDate.setError("Must be after registration end date");
+                isValid = false;
+            }
+
+            if (entrantLimit < 1) {
+                editEntrantLimit.setError("Must be greater than 1");
+                isValid = false;
+            }
+
+            if (!isValid) {
+                return;
+            }
+
+        });
+    }
+
+    private boolean fieldNotEmpty(EditText field) {
+        String value = field.getText().toString().trim();
+        if (value.isEmpty()) {
+            field.setError("Required");
+            return false;
+        }
+        return true;
     }
 }
