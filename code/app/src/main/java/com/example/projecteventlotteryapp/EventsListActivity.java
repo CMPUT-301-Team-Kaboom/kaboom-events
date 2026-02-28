b61062bf3904d378fd2ffe3d61294cc9b22f786a
Elizabeth Seto <135999989+e-seto@users.noreply.github.com>
2026-02-28 11:18:23 -0700

    fix: EventListActivity to make sure it can display

--- code/app/src/main/java/com/example/projecteventlotteryapp/EventsListActivity.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/EventsListActivity.java
@@ -20,7 +20,7 @@ public class EventsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_events_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.events_list_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
