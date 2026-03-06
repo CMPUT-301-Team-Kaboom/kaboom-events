c2fc3a09eff3d844fd8fd96dd589ef9186882986
Ben B <bbui@ualberta.ca>
2026-03-05 21:47:33 -0700

    change: button declaration and scope add: setupUI helper fn

--- code/app/src/main/java/com/example/projecteventlotteryapp/EventDetailsActivity.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/EventDetailsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,25 +22,40 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class EventDetailsActivity extends AppCompatActivity {
+import java.time.format.DateTimeFormatter;
 
+public class EventDetailsActivity extends AppCompatActivity {
     private FirebaseFirestore db;
-    private LinearLayout organizerController;
-    private ConstraintLayout entrantController;
     private String eventId;
     private Event event;
 
-    Button waitlistButton = findViewById(R.id.btn_eventDetails_organizer_waitlist);
-    Button invitedButton = findViewById(R.id.btn_eventDetails_organizer_invited);
-    Button enrolledButton = findViewById(R.id.btn_eventDetails_organizer_enrolled);
-    Button declinedButton = findViewById(R.id.btn_eventDetails_organizer_declined);
+    //=============================
+    // UI Elements
+    //=============================
 
-    Button entrantPrimaryButton = findViewById(R.id.btn_eventDetails_entrant_primary);
-    Button entrantSecondaryButton = findViewById(R.id.btn_eventDetails_entrant_secondary);
+    // organizer Buttons
+    private Button waitlistButton;
+    private Button invitedButton;
+    private Button enrolledButton;
+    private Button declinedButton;
+    private Button editButton;
+    private Button mapButton;
 
-    Button backButton = findViewById(R.id.btn_eventDetails_back)
+    // entrant buttons
+    private Button entrantPrimaryButton;
+    private Button entrantSecondaryButton;
 
+    // global UI elements
+    private ImageButton backButton;
+    private LinearLayout organizerController;
+    private ConstraintLayout entrantController;
 
+    private TextView nameHeaderTextView;
+    private TextView organizerHeaderTextview;
+    private TextView drawDateTV;
+    private TextView registrationPeriodTV;
+    private TextView attendeesTV;
+    private TextView waitListTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +70,7 @@ public class EventDetailsActivity extends AppCompatActivity {
 
         db = FirebaseFirestore.getInstance();
         eventId = getIntent().getStringExtra("eventId");
+        setupUi();
 
         /*  Code adapted from https://firebase.google.com/docs/firestore/query-data/get-data#java */
         DocumentReference docRef = db.collection("events").document(eventId);
@@ -75,8 +92,33 @@ public class EventDetailsActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void setupUi() {
+        waitlistButton  = findViewById(R.id.btn_eventDetails_organizer_waitlist);
+        invitedButton   = findViewById(R.id.btn_eventDetails_organizer_invited);
+        enrolledButton  = findViewById(R.id.btn_eventDetails_organizer_enrolled);
+        declinedButton  = findViewById(R.id.btn_eventDetails_organizer_declined);
+        mapButton       = findViewById(R.id.btn_eventDetails_map);
+
+        entrantPrimaryButton    = findViewById(R.id.btn_eventDetails_entrant_primary);
+        entrantSecondaryButton  = findViewById(R.id.btn_eventDetails_entrant_secondary);
+
+
+        nameHeaderTextView      = findViewById(R.id.tv_eventDetails_event_name_header);
+        organizerHeaderTextview = findViewById(R.id.tv_eventDetails_org_header);
+        drawDateTV              = findViewById(R.id.tv_eventDetails_draw_date);
+        registrationPeriodTV    = findViewById(R.id.tv_eventDetails_registration_period);
+        attendeesTV             = findViewById(R.id.tv_eventDetails_attendees);
+        waitListTV              = findViewById(R.id.tv_eventDetails_waitlist_count);
+
         organizerController = findViewById(R.id.ll_eventDetails_organizer_button_controls);
-        entrantController = findViewById(R.id.cl_eventDetails_entrant_button_controls);
+        entrantController   = findViewById(R.id.cl_eventDetails_entrant_button_controls);
+
+        editButton = findViewById(R.id.btn_eventDetails_edit);
+        backButton = findViewById(R.id.btn_eventDetails_back);
+        backButton.setOnClickListener(v -> finish());
     }
 
     private void configureUIForRole(User user) {
@@ -114,14 +156,20 @@ public class EventDetailsActivity extends AppCompatActivity {
     }
 
     private void updateUi() {
-        TextView nameHeaderTextView = findViewById(R.id.tv_eventDetails_event_name_header);
-        TextView organizerHeaderTextview = findViewById(R.id.tv_eventDetails_org_header);
-        TextView drawDateTV = findViewById(R.id.tv_eventDetails_draw_date);
-        TextView registrationPeriodTV = findViewById(R.id.tv_eventDetails_registration_period);
-        TextView attendeesTV = findViewById(R.id.tv_eventDetails_attendees);
-        TextView waitListTV = findViewById(R.id.tv_eventDetails_waitlist_count);
-
         nameHeaderTextView.setText(event.getName());
+        attendeesTV.setText(String.valueOf(event.getAttendeesLimit()));
+        waitListTV.setText(String.valueOf(event.getWaitlistLimit()));
+
+        DateTimeFormatter drawDatePattern = DateTimeFormatter.ofPattern("MMM d      h:mm a");
+        String formattedDate = event.getDrawDate().format(drawDatePattern).toUpperCase();
+        drawDateTV.setText(formattedDate);
+
+        DateTimeFormatter registrationPeriodPattern = DateTimeFormatter.ofPattern("MMM d");
+        String registrationPeriodText = String.format("%s - %s",
+                event.getRegistrationStartDate().format(registrationPeriodPattern).toUpperCase(),
+                event.getRegistrationEndDate().format(registrationPeriodPattern).toUpperCase()
+        );
+        registrationPeriodTV.setText(registrationPeriodText);
 
 
         // temp user
@@ -129,9 +177,5 @@ public class EventDetailsActivity extends AppCompatActivity {
         // show only specific buttons for role
         configureUIForRole(user);
     }
-//
-//    private void openUserList(String tempVar) {
-//        // todo
-//    }
 }
 
