61f9b1846859df45fa23618b70c23019e54a80f8
Anna Kibria <annakibria@gmail.com>
2026-03-12 11:08:41 -0600

    javadoc comments to EntrantList

--- code/app/src/main/java/com/example/projecteventlotteryapp/EntrantList.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/EntrantList.java
@@ -19,10 +19,19 @@ public class EntrantList implements Serializable {
         listLength = 0;
     }
 
+
+    /**
+     * Gets the entrants
+     * @return entrants arraylist
+     */
     public ArrayList<User> getEntrants() {
         return entrants;
     }
 
+    /**
+     * add an entrant to the list
+     * @param entrant entrant to add type User
+     */
     public void addEntrant(User entrant) {
         if (entrant.getRole() != Role.ENTRANT) {
             throw new IllegalArgumentException("User must have role ENTRANT");
@@ -32,6 +41,10 @@ public class EntrantList implements Serializable {
         listLength++;
     }
 
+    /**
+     * remove an entrant from the list
+     * @param entrant entrant to remove type User
+     */
     public void removeEntrant(User entrant) {
         if (entrant.getRole() != Role.ENTRANT) {
             throw new IllegalArgumentException("User must have role ENTRANT");
@@ -43,10 +56,19 @@ public class EntrantList implements Serializable {
 
 //    public popEntrant(Entrant entrant)
 
+    /**
+     * check if a user is in the list
+     * @param user User object to check for in the entrants list
+     * @return true if user is in the list, false otherwise
+     */
     public boolean contains(User user) {
         return entrants.contains(user);
     }
 
+    /**
+     * gets the length of the list
+     * @return listLength
+     */
     public int getListLength() {
         return listLength;
     }
