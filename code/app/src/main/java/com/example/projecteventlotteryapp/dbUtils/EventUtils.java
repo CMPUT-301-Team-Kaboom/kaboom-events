a8c90fe95181bd246e13ae34c42bbf6ce0ea5e56
alyssaS <alyssashens@gmail.com>
2026-03-24 16:29:40 -0600

    initiate coorganizer in events

--- code/app/src/main/java/com/example/projecteventlotteryapp/dbUtils/EventUtils.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/dbUtils/EventUtils.java
@@ -241,6 +241,7 @@ public class EventUtils {
 
         // get array field
         ArrayList<String> tagsList = FirestoreUtils.fetchStringArrayList(snapshot, "tags");
+        ArrayList<String> coorganizerIds = FirestoreUtils.fetchStringArrayList(snapshot, "coorganizers");
 
         DocumentReference organizer = snapshot.getDocumentReference("organizer");
 
@@ -260,6 +261,7 @@ public class EventUtils {
         event.setGeolocationEnabled(geolocationEnabled);
         event.setTagsList(tagsList);
         event.setWaitlistLimit(waitlistLimit);
+        event.setCoorganizerIds(coorganizerIds != null ? coorganizerIds : new ArrayList<>());
 
         Log.d("Event", "Fetched event.\nEventId: " + eventId + "\nname: " + name);
         return event;
@@ -337,6 +339,7 @@ public class EventUtils {
         eventData.put("invited",  new ArrayList<>());
         eventData.put("declined", new ArrayList<>());
         eventData.put("comments", new ArrayList<>());
+        eventData.put("coorganizers", new ArrayList<>());
 
         db.collection("events")
                 .add(eventData)
@@ -541,5 +544,25 @@ public class EventUtils {
     public void deleteCommentFromEvent(String eventId, String commentId){
         db.collection("comments").document(commentId).delete();
         db.collection("events").document(eventId).update("comments", FieldValue.arrayRemove(commentId));
+    }
+
+    /**
+     * Adds a co-organizer to an event.
+     * @param eventId the ID of the event
+     * @param userId the ID of the user to be added as a co-organizer
+     * @return a Task representing the asynchronous operation
+     */
+    public Task<Void> addCoorganizer(String eventId, String userId) {
+        return db.collection("events").document(eventId).update("coorganizers", FieldValue.arrayUnion(userId));
+    }
+
+    /**
+     * Removes a co-organizer from an event.
+     * @param eventId the ID of the event
+     * @param userId the ID of the user to be removed as a co-organizer
+     * @return a Task representing the asynchronous operation
+     */
+    public Task<Void> removeCoorganizer(String eventId, String userId) {
+        return db.collection("events").document(eventId).update("coorganizers", FieldValue.arrayRemove(userId));
     }
 }
