3d02ef8362ffa8f4f81ea0966a16ed09c1807890
Elizabeth Seto <135999989+e-seto@users.noreply.github.com>
2026-03-25 11:44:39 -0600

    remove: unused method

--- code/app/src/main/java/com/example/projecteventlotteryapp/Models/EventsFilter.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/Models/EventsFilter.java
@@ -109,21 +109,4 @@ public class EventsFilter {
 
         return true;
     }
-
-    public boolean isAvailable(Event event, QueryDocumentSnapshot snapshot, LocalDate today) {
-        // check registration period still available
-        if (today.isBefore(event.getRegistrationStartDate()) || today.isAfter(event.getRegistrationEndDate())) {
-            return false;
-        }
-
-        // check waitlist limit not surpassed
-        int waitlistSize = (int) snapshot.get("waitlistSize");
-        int waitlistLimit = (int) snapshot.get("waitlistLimit");
-        if (waitlistSize >= waitlistLimit) {
-            return false;
-        }
-
-        return true;
-    }
-
 }
