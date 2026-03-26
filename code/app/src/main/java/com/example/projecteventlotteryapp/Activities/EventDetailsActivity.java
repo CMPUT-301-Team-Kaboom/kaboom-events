569e60b0e590520415fa1a87b1bc9072a76ba8af
Kevin Cao <kcao36@outlook.comgit config --global user.email kcao36@outlook.comclear>
2026-03-25 21:16:25 -0600

    adjust to new dev code

--- code/app/src/main/java/com/example/projecteventlotteryapp/Activities/EventDetailsActivity.java
+++ code/app/src/main/java/com/example/projecteventlotteryapp/Activities/EventDetailsActivity.java
@@ -614,15 +614,40 @@ public class EventDetailsActivity extends AppCompatActivity {
     private void drawEntrantsForInvitationList() {
         eventUtils.generateInvitationList(event.getEventId(), event.getAttendeesLimit())
                 .addOnSuccessListener(aVoid -> {
-                    if (globalUser.getRole() == Role.ORGANIZER) {
-                        Toast.makeText(this, "Draw Complete", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    Log.e("EventDetailsActivity", "Failed to generate invitationList. Error: " + e);
-                    if (globalUser.getRole() == Role.ORGANIZER) {
-                        Toast.makeText(this, "Could not complete Draw", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(this, "Draw Complete", Toast.LENGTH_SHORT).show();
+
+                    // fetch invitation list
+                    db.collection("events").document(eventId).get()
+                            .addOnSuccessListener(doc -> {
+                                ArrayList<String> invited = (ArrayList<String>) doc.get("invited");
+
+                                if (invited != null && !invited.isEmpty()) {
+                                    String organizerId = globalUser.getUserId();
+
+                                    String message = "Congrats! You are invited to this event";
+
+                                    // iterate through invited list and send invitations
+                                    for (String invitedId : invited) {
+                                        FirestoreUtils.storeNotificationInFirestore(
+                                                organizerId,
+                                                invitedId,
+                                                message,
+                                                event.getName(),
+                                                db
+                                        );
+                                        Log.d("EventDetails", "Automated notifications sent to " + invited.size() + " winners.");
+                                    }
+                                }
+
+                            })
+                            .addOnFailureListener(e -> {
+                                Log.e("EventDetailsActivity", "Failed to generate invitationList. Error: " + e);
+                                Toast.makeText(this, "Could not complete Draw", Toast.LENGTH_SHORT).show();
+                            });
                 });
+
     }
+
 }
+
+
